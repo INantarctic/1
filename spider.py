974df72aca7831e94aef0f96c4de4817a243dc5b
INantarctic <38390476+INantarctic@users.noreply.github.com>
2018-04-15 10:56:30 +0800

    Add files via upload

---
+++ spider.py
@@ -0,0 +1,91 @@
+"""
+爬蟲前奏:
+
+
+明確目的
+找到數據對應的網頁
+
+分析網頁的結構找到數據所在的標簽位置
+
+
+
+模擬HTTP請求,和服務器發送這個講求,獲取到服務器返回給我們的HTML
+
+用正則表達式提取我們要的數據(名字,人氣)
+
+"""
+import re
+from urllib import request
+#斷點調適
+
+
+class Spider():
+    url = 'https://www.panda.tv/cate/lol'
+    root_pattern = '<div class="video-info">([\s\S]*?)</div>'
+    name_pattern = '</i>([\s\S]*?)</span>'
+    number_pattern = '<span class="video-number">([\s\S]*?)</span>'
+
+     
+    def __fetch_content(self):
+        r = request.urlopen(Spider.url)
+        #htmls 為bytes
+        htmls = r.read()
+        htmls = str(htmls, encoding='utf-8')
+        
+        return htmls
+
+    def __analysis(self,htmls):
+        root_html = re.findall(Spider.root_pattern,htmls)
+        
+        anchors = []
+        for html in root_html:
+            name = re.findall(Spider.name_pattern,html)
+            number = re.findall(Spider.number_pattern,html)
+            anchor = {'name':name,'number':number}
+            anchors.append(anchor)
+            
+        return anchors 
+
+    def __refine(self,anchors):
+        l = lambda anchor:{
+            'name':anchor['name'][0].strip(),
+            'number':anchor['number'][0]
+            }
+        
+        return map(l,anchors)
+
+    def __sort(self,anchors):
+        anchors = sorted(anchors,key=self.__sort_seed,reverse=True)
+        
+        return anchors
+
+    def __sort_seed(self,anchor):
+        r = re.findall('\d*',anchor['number'])
+        number = float(r[0])
+        if '万' in anchor['number']:
+            number *= 10000
+            
+        return number
+
+    def __show(self,anchors):
+        for rank in range(0,len(anchors)):
+            print('rank  ' + str(rank+1)
+                  +  ': ' + anchors[rank]['name']
+                  + '  ' + anchors[rank]['number'])
+        
+
+
+    def go(self):
+        htmls = self.__fetch_content()
+        anchors = self.__analysis(htmls)
+        anchors = list(self.__refine(anchors))
+        anchors = self.__sort(anchors)
+        anchors = self.__show(anchors)
+        
+
+
+
+ 
+spider = Spider()
+spider.go()
+
